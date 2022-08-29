71c981ff4aad99f534db27a7d31549aace7322a4
0xkiichiro <barisaytimur@gmail.com>
2022-08-29 22:21:31 +0300

    create & update / css & bootstap, static

---
+++ 011-CRUD_project/todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import home, todo_create, todo_update
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("add/", todo_create, name="create"),
+    path("update/<int:id>", todo_update, name="update"),
+]

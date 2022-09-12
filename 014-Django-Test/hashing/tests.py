cc9f3b7a329ca316c223405d1556c62947907454
0xkiichiro <barisaytimur@gmail.com>
2022-09-12 22:05:47 +0300

    unit tests

--- 014-Django-Test/hashing/tests.py
+++ 014-Django-Test/hashing/tests.py
@@ -1,3 +1,43 @@
+from unittest import TestCase
+from selenium import webdriver
 
-        
+browser = webdriver.Chrome()
+browser.get('http://localhost:8000')
+
+# class FunctionalTestCase(TestCase):
     
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
+
+#     def tearDown(self):
+#         self.browser.quit()
+
+#     # to thest only one test => 
+#     # python manage.py test hashing.tests.FunctionalTestCase.test_there_is_homepage
+#     def test_there_is_homepage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('Enter hash here', self.browser.page_source)
+
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id("id_text")
+#         text.send_keys("hello")
+#         self.browser.find_element_by_name("submit").click()
+#         self.assertInHTML("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", self.browser.page_source)
+
+from .forms import HashForm
+
+class UnitTestCase(TestCase):
+
+
+    def test_home_homepage_tempalte(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')
+
+    # There should be a form the user enters a text to 
+    # find corresponding hash
+    def test_hash_form(self):
+        # Check if there is a form, needs to filled with some data, for not to be an empty form which basically equals to False
+        form = HashForm(data={'text': 'hello'})
+        # Check if it is valid
+        self.assertTrue(form.is_valid())
